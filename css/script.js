eb16414160d568a9aad69a58e25e99ef7971114e
Inaldo Macena <inaldomacena19@gmail.com>
2023-09-09 05:34:48 -0300

    atualização

---
+++ css/script.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+const hamburguer = document.querySelector(".hamburguer");
+const  navMenu  = document.querySelector(".nav-menu");
+
+hamburguer.addEventListener("click",() => {
+    hamburguer.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
+
+
+
+
+
